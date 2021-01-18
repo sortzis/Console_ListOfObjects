a06f239bf62238bc35518b9aacdb4c9eea1454b8
Samantha Sortzi <sortzis@mail.nmc.edu>
2021-01-18 11:14:33 -0500

    initial push

---
+++ Console_ListOfObjects/Animal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ListOfObjects
+{
+    class Animal
+    {
+
+        public enum Diet
+        {
+            none,
+            omnivore,
+            herbivore,
+            carnivore
+        }
+
+        #region FIELDS
+
+        private string _name;
+        private int _leg;
+        private Diet _diets;
+        private bool _status;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Leg
+        {
+            get { return _leg; }
+            set { _leg = value; }
+        }
+
+        public Diet Diets
+        {
+            get { return _diets; }
+            set { _diets = value; }
+        }
+
+        private bool Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ Console_ListOfObjects/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ListOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            AddAnimals(animals);
+            DisplayAnimals(animals);
+        }
+
+        private static void DisplayAnimals(List<Animal> animals)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\tDisplay Animals");
+            Console.WriteLine();
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine($"Animal Name: {animal.Name}");
+            }
+
+        }
+
+        private static void AddAnimals(List<Animal> animals)
+        {
+            Animal animal = new Animal();
+
+            Console.WriteLine();
+            Console.WriteLine("\t\tAnimals Application");
+            Console.WriteLine("\t\tAdd Animals to List Below!");
+            Console.WriteLine();
+
+            bool doneAdding = false;
+            do
+            {
+                Console.Write("Name: ");
+                animal.Name = Console.ReadLine();
+                Console.Write("Legs: ");
+                animal.Leg = int.Parse(Console.ReadLine());
+                Console.Write("Diet (Carnivore, Herbivore, Omnivore?): ");
+                Enum.TryParse(Console.ReadLine().ToLower(), out Animal.Diet diet);
+                animal.Diets = diet;
+
+                animals.Add(animal);
+
+                Console.Write("Add more animals?:");
+                string userResponse = Console.ReadLine().ToLower();
+                if (userResponse == "no")
+                {
+                    doneAdding = true;
+                }
+
+            } while (!doneAdding);
+
+        }
+    }
+}
